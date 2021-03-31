23b5e9fee7adee6dcd16204524e8e6255b3265cb
Jesse <jessevanpapenrecht@quicknet.nl>
2021-03-31 09:10:47 +0200

    Added tweet Bitcoin price function

---
+++ main.py
@@ -0,0 +1,19 @@
+from twython import Twython
+from bitcoin import price
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+message = "Bitcoin is currently $" + price
+twitter.update_status(status=message)
+print("Tweeted: {}".format(message))
